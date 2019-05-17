fd5036de220f0449d998dd385ba178169be09f7d
njsdias <njsdias@gmail.com>
2019-05-17 17:01:58 +0100

    Add files

--- Tutorial1/app.py
+++ Tutorial1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask                         # import the require libraries
+from flask import Flask , jsonify, request      # import the require libraries
 app = Flask(__name__)                           # start the Flask application calling any name
 
 @app.route('/')                                 # when the webserver see this endpoint the flask get the request the def hello_world
@@ -17,7 +17,41 @@ def bye():
 #    c = 1/0                                   # it will give us a Internal Server Error. Run it with app.run(debug=True)
     return "bye"
 
+@app.route('/jsexample')
+def jsexample():
+    age = 2*5
+    retJson = {
+        'Name': 'Ochenback',
+        'Age': age,
+        'phones':[
+            {
+                "phoneName": "Iphone8",
+                "phoneNumber": 11111
+            },
+            {
+                "phoneName": "Nokia",
+                "phoneNumber": 11121
+            }
 
+        ]
+    }
+    return jsonify(retJson)
+
+@app.route('/add_two_nums', methods=["POST"]) # this only allow the POST method
+def add_two_nums():
+    # Get x,y from the posted data
+    dataDict = request.get_json()    # here we get the post information
+    x = dataDict["x"]                # here we take the "x" value posted
+    y = dataDict["y"]                # here we take the "x" value posted
+    #Add z=x+y
+    z = x + y                        # here we add_two_nums
+    #Prepare a JSON, "z":z           # here we prepare the JSON structure to return the JSON
+    retJson = {
+        "z":z
+    }
+    #return jsonify(map_prepared)
+    return jsonify(retJson), 200     # This code is to infor to the user the success of the request
+                                     # The 404 code is for the FAIL Request
 
 if __name__=="__main__":
     app.run()
